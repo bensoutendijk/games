629659e189b604a9b61d8d94af21619f778ff27a
Ben Soutendijk <bensoutendijk@gmail.com>
2022-06-06 23:29:36 -0500

    auto import from aws

--- docker/minecraft.dockerfile
+++ docker/minecraft.dockerfile
@@ -1,1 +1,12 @@
 FROM itzg/minecraft-server
+
+RUN apt-get update \
+  && apt-get install -y curl unzip
+
+RUN curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip \
+  && unzip awscliv2.zip \
+  && ./aws/install \
+  && rm -rf aws awscliv2.zip
+
+
+ENTRYPOINT [ "bash", "/startWithImport.sh" ]
---
+++ scripts/startMinecraft.sh
@@ -0,0 +1,33 @@
+WORLD=$(grep level-name /config/server.properties | cut -d '=' -f 2-)
+
+echo "Checking if game data exists in '/data/${WORLD}'"
+
+if [ -d "/data/${WORLD}" ] 
+then
+    echo "Level already exists in volume."
+else
+    echo "Fetching game data from AWS..."
+    FILENAME=$(aws s3 ls bgoodmon/games/minecraft/ | grep ${WORLD} | sort -r | head -1 | awk '{print $4}')
+
+    if [ ! -z "$FILENAME" ]
+    then
+        mkdir -p /tmp
+        aws s3 cp s3://bgoodmon/games/minecraft/${FILENAME} /tmp/${FILENAME}
+
+        echo "Extracting data..."
+        mkdir -p /data/${WORLD}
+        tar -xvf /tmp/${FILENAME} -C /data/${WORLD}
+
+        chown 1000:1000 /data/${WORLD}
+
+        echo "Syncing server config with git repo"
+        rsync -a /config/ /data/
+
+        echo "Cleaning up..."
+        rm /tmp/${FILENAME}
+    else
+        echo "No backups found for ${WORLD}. New world will be created..."
+    fi
+fi
+
+bash /start
